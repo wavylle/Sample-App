5f823e65ab58fc5afdc16fdaa940a08430982165
Abhirath Dubey <abhirathdubey2804@gmail.com>
2022-11-12 16:47:56 +0530

    first commit

---
+++ sampleapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import random
+
+@csrf_exempt
+def testpost(request):
+    if request.method == 'POST':
+        a = request.POST.get('a')
+        b = request.POST.get('b')
+
+        a = int(a)
+        b = int(b)
+
+        result = a + b
+        json_data = {'answer': result}
+        return JsonResponse(result)
